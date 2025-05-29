4ef6f7a5d7267201978ec7819b641992ccf61239
vanessa.prado <neh.prado@gmail.com>
2025-05-29 16:43:34 -0400

    gerando o eb

---
+++ build.sh
@@ -0,0 +1,12 @@
+
+versao=$(git rev-parse HEAD | cut -c 1-7)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 448522291635.dkr.ecr.us-east-1.amazonaws.com
+docker build -t bia .
+docker tag bia:latest 448522291635.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+docker push 448522291635.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+rm .env
+./gerar-compose.sh
+rm bia-versao*.zip
+zip bia-versao-$versao.zip docker-compose.yml
+git checkout docker-compose.yml
+
