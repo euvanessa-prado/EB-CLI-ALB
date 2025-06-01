1f206ecbd348813d633bc0989431ba0201f2d937
vanessa.prado <neh.prado@gmail.com>
2025-06-01 17:24:16 -0400

    alteracao

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ docker compose -f docker-compose-build-eb.yml build bia
 docker tag bia:latest 448522291635.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
 docker push 448522291635.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
 rm .env 2> /dev/null
-checar_ultimo_comando
 ./gerar-compose.sh
-checar_ultimo_comando
 rm bia-versao.zip
 zip bia-versao.zip docker-compose.yml
 git checkout docker-compose.yml
