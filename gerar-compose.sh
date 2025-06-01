632ca7e7fb066bee9232384ff2af656ec1f7ff5a
vanessa.prado <neh.prado@gmail.com>
2025-06-01 01:29:35 -0400

    add

--- gerar-compose.sh
+++ gerar-compose.sh
@@ -3,7 +3,7 @@ versao=$(git rev-parse HEAD | cut -c 1-7)
 
 echo TAG=$versao >> .env
 
-docker compose -f docker-compose-eb.yml config --no-normalize >> docker-compose-dev.yml
+docker compose -f docker-compose-eb.yml config --no-normalize > docker-compose-dev.yml
 sed -i '/^name:/d' docker-compose-dev.yml
 sed -i '/bia:$/a \    env_file: .env' docker-compose-dev.yml
 
